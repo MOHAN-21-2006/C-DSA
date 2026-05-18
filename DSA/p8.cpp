11d093ae09cada89e07f0d27e011fd23b11216a8
MOHAN-21-2006 <legendmax08@gmail.com>
2026-05-18 22:30:11 +0530

    array

--- DSA/p8.cpp
+++ DSA/p8.cpp
@@ -1,1 +1,20 @@
-#include
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int arr[5] = {4, 8, 1, 9, 3};
+
+    int largest = arr[0];
+
+    for(int i = 1; i < 5; i++) {
+
+        if(arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest Element = " << largest;
+
+    return 0;
+}
